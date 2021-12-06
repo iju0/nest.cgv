0b796d9ab7e1d6f0ed5576c61baa64027225e325
idev <idev>
2021-12-06 23:53:43 +0900

    Create Entity, Dto

--- src/bill/dto/create-bill.dto.ts
+++ src/bill/dto/create-bill.dto.ts
@@ -1,1 +1,33 @@
-export class CreateBillDto {}
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateBillDto {
+
+  @IsNotEmpty()
+  @IsString()
+  gubun: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number;
+
+  @IsNotEmpty()
+  @IsString()
+  paymentMethod: string;
+
+  @IsNotEmpty()
+  @IsString()
+  paymentNumber: string;
+
+  @IsNotEmpty()
+  @IsString()
+  paymentValidate: string;
+
+  @IsNotEmpty()
+  @IsString()
+  paymentOwner: string;
+
+  @IsNotEmpty()
+  @IsString()
+  paymentContact: string;
+}
