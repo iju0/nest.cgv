5868a295b916dce10ff2f534007109113fdbbb81
idev <idev>
2021-12-08 21:55:28 +0900

    Bill Service 구현 완료

--- src/bill/bill.module.ts
+++ src/bill/bill.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BillService } from './bill.service';
 import { BillController } from './bill.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bill } from './entities/bill.entity';
 
 @Module({
   controllers: [BillController],
-  providers: [BillService]
+  providers: [BillService],
+  imports: [TypeOrmModule.forFeature([Bill])],
 })
 export class BillModule {}
--- src/bill/bill.service.ts
+++ src/bill/bill.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBillDto } from './dto/create-bill.dto';
 import { UpdateBillDto } from './dto/update-bill.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Bill } from './entities/bill.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BillService {
-  create(createBillDto: CreateBillDto) {
-    return 'This action adds a new bill';
+  constructor(
+    @InjectRepository(Bill)
+    private readonly billRepository: Repository<Bill>,
+  ) {}
+  async create(createBillDto: CreateBillDto) {
+    const bill = await this.billRepository.create(createBillDto);
+    return await this.billRepository.save(bill);
   }
 
-  findAll() {
-    return `This action returns all bill`;
+  async findAll() {
+    return await this.billRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} bill`;
+  async findOne(id: number) {
+    return await this.billRepository.findOne(id);
   }
 
-  update(id: number, updateBillDto: UpdateBillDto) {
-    return `This action updates a #${id} bill`;
+  async update(id: number, updateBillDto: UpdateBillDto) {
+    const bill = await this.findOne(id);
+
+    if (!bill) {
+      throw new Error('데이터를 찾을 수 없습니다.');
+    }
+
+    bill.gubun = updateBillDto.gubun;
+    bill.amount = updateBillDto.amount;
+    bill.paymentContact = updateBillDto.paymentContact;
+    bill.paymentMethod = updateBillDto.paymentMethod;
+    bill.paymentNumber = updateBillDto.paymentNumber;
+    bill.paymentOwner = updateBillDto.paymentOwner;
+    bill.paymentValidate = updateBillDto.paymentValidate;
+
+    return await this.billRepository.save(bill);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} bill`;
+  async remove(id: number) {
+    const bill = await this.findOne(id);
+    return await this.billRepository.remove(bill);
   }
 }
